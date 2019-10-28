bcc6d836097c8e6dd376e85a25865d1f8390e291
longdavid2k17 <kantochdawid98@gmail.com>
2019-10-28 17:45:08 +0100

    TextEditor.java
    Little refresh, added some more font sizes

--- TextEditor.java
+++ TextEditor.java
@@ -40,7 +40,7 @@ public class TextEditor extends JFrame implements ActionListener
     PrinterJob printerJob;
 
     private static final List<String> FONT_LIST = Arrays.asList(new String [] {"Arial", "Calibri", "Cambria", "Courier New", "Comic Sans MS", "Dialog", "Georgia", "Helevetica", "Lucida Sans", "Monospaced", "Tahoma", "Times New Roman", "Verdana"});
-    private static final String[] FONT_SIZES = {"7","8","9","10","11","12","13","14","15","16","17","18","19","20","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50"};
+    private static final String[] FONT_SIZES = {"7","8","9","10","11","12","13","14","15","16","17","18","19","20","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60"};
     private static final int DEFAULT_FONT_SIZE = 10;
 
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
